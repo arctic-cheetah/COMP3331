d8ed565ae5268ed9e70e5f56feeea35416b59258
arctic-cheetah <gondonlondon@gmail.com>
2024-03-06 13:52:54 +1100

    Cleaned up lab03

--- lab03/WebServer.py
+++ lab03/WebServer.py
@@ -1,8 +1,8 @@
-import sys, socket, random, time, functools, os, threading
+import sys, socket, os, threading
 
-# HOST_NAME = 
+# HOST_NAME =
 hostName = "127.0.0.1"
-CHUNK_SIZE = 0xfff
+CHUNK_SIZE = 0xFFF
 decoder = "ascii"
 HTTP_OK = "200 OK"
 HTTP_NO_CONTENT = "204 No Content"
@@ -13,60 +13,62 @@ TXT = "text/html"
 
 
 def main():
-    
 
-    
-    
-    if (len(sys.argv) < 2):
+    if len(sys.argv) < 2:
         print("Required Arguments: [port]")
         return 0
     port = int(sys.argv[1])
     # hostName = socket.gethostbyname(socket.gethostname()) # TEST!
     address = (hostName, port)
-    #i) Create a connection socket when contacted by a client (browser).
+    # i) Create a connection socket when contacted by a client (browser).
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(address)
     server.listen()
 
-    #(ii) Receive HTTP requests from this connection. Your server should only process GET requests. You may assume that only GET requests will be received.
+    # (ii) Receive HTTP requests from this connection. Your server should only process GET requests. You may assume that only GET requests will be received.
     while True:
         (sock, addr) = server.accept()
         thread = threading.Thread(target=handle_client, args=(sock, addr, port))
         thread.start()
         print(f"[Active Connections] {threading.active_count() - 1}")
-        
 
 
+def HTTP_response(code: str, content: str, length: int):
+    return (
+        HTTP_RESPONSE_TOP
+        + " "
+        + code
+        + "\n\r"
+        + f"""Content-Type: {content}\n\rContent-Length: {length}\n\rKeep-Alive: timeout=2, max=2\r\n\r\n"""
+    )
 
-def HTTP_response(code : str, content: str, length: int):
-    return HTTP_RESPONSE_TOP + " " + code + "\n\r" + f"""Content-Type: {content}\n\rContent-Length: {length}\n\rKeep-Alive: timeout=2, max=2\r\n\r\n"""
 
-def handle_client(sock : socket, addr, serverPort :int):
-    #(iii) Parse the request to determine the specific file being requested.
-    
+def handle_client(sock: socket, addr, serverPort: int):
+    # (iii) Parse the request to determine the specific file being requested.
+
     connected = True
     while connected:
-        msg : bytes = sock.recv(CHUNK_SIZE)
+        msg: bytes = sock.recv(CHUNK_SIZE)
         HTTP_data = msg.decode(decoder).split("\r\n")
         HTTP_req = HTTP_data[0]
-        #(iv) Get the requested file from the server's file system.
-        request = HTTP_req.split(' ')[0]
+        # (iv) Get the requested file from the server's file system.
+        request = HTTP_req.split(" ")[0]
         print(HTTP_req)
-        if (len(msg) == 0):
+        if len(msg) == 0:
             connected = False
             sock.close()
             return
-        
-        if (request == "GET"):
+
+        if request == "GET":
             try:
-                fileName = HTTP_req.split(' ')[1].strip("/")
+                fileName = HTTP_req.split(" ")[1].strip("/")
                 (name, extension) = fileName.split(".")
-                sendFileData(sock, fileName, extension)   
-                #(v) Create an HTTP response message consisting of the requested file preceded by header lines.
-                #(vi) Send the response over the TCP connection to the requesting browser.
+                sendFileData(sock, fileName, extension)
+                # (v) Create an HTTP response message consisting of the requested file preceded by header lines.
+                # (vi) Send the response over the TCP connection to the requesting browser.
             except Exception as err:
-                res : bytes
-                if (str(err) == "ico"):
+                res: bytes
+                if str(err) == "ico":
                     res = HTTP_response(HTTP_NO_CONTENT, TXT, 0).encode()
                     # sock.close()
                     # connected = False
@@ -75,40 +77,36 @@ def handle_client(sock : socket, addr, serverPort :int):
                 print(res)
                 sock.send(res)
 
-                
-                            
-        #(vii) If the requested file is not present on the server, the server should send an HTTP “404 Not Found” message back to the client.
-        #(viii) The server should listen in a loop, waiting for the next request from the browser.
-        #(ix) The server should be able to handle HTTP 1.1 persistent connections. This means It should be able to handle multiple requests from the same connection. This will carry 1 mark if you manage to implement this
+        # (vii) If the requested file is not present on the server, the server should send an HTTP “404 Not Found” message back to the client.
+        # (viii) The server should listen in a loop, waiting for the next request from the browser.
+        # (ix) The server should be able to handle HTTP 1.1 persistent connections. This means It should be able to handle multiple requests from the same connection. This will carry 1 mark if you manage to implement this
     sock.close()
 
-def sendFileData(sock, fileName, extension):
-    debug=""
-    if (os.getcwd() == '/home/k-730/Code/COMP3331'):
-        debug = "lab03/"
 
-    if (extension == "html"):
-        f = open(debug + fileName, "r")
+def sendFileData(sock, fileName, extension):
+
+    if extension == "html":
+        f = open(fileName, "r", encoding="utf-8")
         res = f.read()
         http = HTTP_response(HTTP_OK, TXT, f.tell()) + res
         f.close()
         sock.send(http.encode())
         print(http)
-    elif (extension == "png"):
-        f = open(debug + fileName, "rb")
+    elif extension == "png":
+        f = open(fileName, "rb")
         res = f.read()
-        http = HTTP_response(HTTP_OK, PNG, f.tell()) # + f"<img src={hostName}:{serverPort}/{fileName}>"
+        http = HTTP_response(
+            HTTP_OK, PNG, f.tell()
+        )  # + f"<img src={hostName}:{serverPort}/{fileName}>"
         sock.send(http.encode())
         sock.send(res)
         f.close()
         print(http)
-    elif (extension == "ico"):
+    elif extension == "ico":
         raise Exception("ico")
     else:
         raise Exception("Incompatible file type")
-        
-        
-    
+
 
 if __name__ == "__main__":
     main()
--- lab03/lab03.md
+++ lab03/lab03.md
@@ -20,9 +20,29 @@ dig www.princeton.edu
 ;www.princeton.edu.             IN      A
 
 ;; ANSWER SECTION:
-www.princeton.edu.      2501    IN      CNAME   www.princeton.edu.cdn.cloudflare.net.
-www.princeton.edu.cdn.cloudflare.net. 300 IN A  104.18.4.101
-www.princeton.edu.cdn.cloudflare.net. 300 IN A  104.18.5.101
+www.princeton.edu.      2707    IN      CNAME   www.princeton.edu.cdn.cloudflare.net.
+www.princeton.edu.cdn.cloudflare.net. 174 IN A  104.18.5.101
+www.princeton.edu.cdn.cloudflare.net. 174 IN A  104.18.4.101
+
+;; AUTHORITY SECTION:
+cloudflare.net.         79426   IN      NS      ns3.cloudflare.net.
+cloudflare.net.         79426   IN      NS      ns5.cloudflare.net.
+cloudflare.net.         79426   IN      NS      ns4.cloudflare.net.
+cloudflare.net.         79426   IN      NS      ns2.cloudflare.net.
+cloudflare.net.         79426   IN      NS      ns1.cloudflare.net.
+
+;; ADDITIONAL SECTION:
+ns1.cloudflare.net.     766     IN      A       173.245.59.31
+ns1.cloudflare.net.     766     IN      AAAA    2400:cb00:2049:1::adf5:3b1f
+ns2.cloudflare.net.     48      IN      A       198.41.222.131
+ns2.cloudflare.net.     48      IN      AAAA    2400:cb00:2049:1::c629:de83
+ns3.cloudflare.net.     320     IN      A       198.41.222.31
+ns3.cloudflare.net.     320     IN      AAAA    2400:cb00:2049:1::c629:de1f
+ns4.cloudflare.net.     509     IN      A       198.41.223.131
+ns4.cloudflare.net.     509     IN      AAAA    2400:cb00:2049:1::c629:df83
+ns5.cloudflare.net.     110     IN      A       198.41.223.31
+ns5.cloudflare.net.     110     IN      AAAA    2400:cb00:2049:1::c629:df1f
+
 
 ;; Query time: 12 msec
 ;; SERVER: 129.94.242.2#53(129.94.242.2) (UDP)
@@ -41,13 +61,14 @@ The DNS query is of type 'A' record.
 The canonical name is [www.princeton.edu.cdn.cloudflare.net.](www.princeton.edu.cdn.cloudflare.net.)
 Benefits for Princeton University for using CNAME(alias):
 
+- Makes it easier to remember the url by exluding the trailing cloudflare.net
 - allows the creation of subdomains. Such as net.princeton.edu or oit.princton.edu
 - enables the use of external services such as cloudflare to prevent network attacks
-- Makes it easier to remember the url by exluding the trailing cloudflare.net
+- Allows easier substition of IP address
 
 ### 3.3) What can you make of the rest of the response/what is it used for (i.e., the details available in the DNS response (cookies and other fields))?
 
-On my current VLAB machine, the rest of the response shows that:
+On my current machine, the rest of the response shows that:
 
 - I had 1 query
 - I received 3 answers, 0 from authority and 1 from additional
@@ -55,56 +76,11 @@ On my current VLAB machine, the rest of the response shows that:
 - Followed by the answer question to my DNS query
 - Lastly, the query time, server IP and date and message size of the query are at the end.
 
-However, on my personal machine, we the additional and authority section included:
-
-```
-; <<>> DiG 9.19.17-2~kali1-Kali <<>> www.princeton.edu
-;; global options: +cmd
-;; Got answer:
-;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 8290
-;; flags: qr rd ra; QUERY: 1, ANSWER: 3, AUTHORITY: 5, ADDITIONAL: 11
-
-;; OPT PSEUDOSECTION:
-; EDNS: version: 0, flags:; udp: 1232
-; COOKIE: 0ca7b547b54524e7e9fe9a4665e4050df8ff1e218da6fcc5 (good)
-;; QUESTION SECTION:
-;www.princeton.edu.             IN      A
-
-;; ANSWER SECTION:
-www.princeton.edu.      3600    IN      CNAME   www.princeton.edu.cdn.cloudflare.net.
-www.princeton.edu.cdn.cloudflare.net. 300 IN A  104.18.4.101
-www.princeton.edu.cdn.cloudflare.net. 300 IN A  104.18.5.101
-
-;; AUTHORITY SECTION:
-cloudflare.net.         111793  IN      NS      ns2.cloudflare.net.
-cloudflare.net.         111793  IN      NS      ns4.cloudflare.net.
-cloudflare.net.         111793  IN      NS      ns5.cloudflare.net.
-cloudflare.net.         111793  IN      NS      ns1.cloudflare.net.
-cloudflare.net.         111793  IN      NS      ns3.cloudflare.net.
-
-;; ADDITIONAL SECTION:
-ns1.cloudflare.net.     491     IN      A       173.245.59.31
-ns2.cloudflare.net.     491     IN      A       198.41.222.131
-ns3.cloudflare.net.     491     IN      A       198.41.222.31
-ns4.cloudflare.net.     491     IN      A       198.41.223.131
-ns5.cloudflare.net.     491     IN      A       198.41.223.31
-ns1.cloudflare.net.     111793  IN      AAAA    2400:cb00:2049:1::adf5:3b1f
-ns2.cloudflare.net.     111793  IN      AAAA    2400:cb00:2049:1::c629:de83
-ns3.cloudflare.net.     111793  IN      AAAA    2400:cb00:2049:1::c629:de1f
-ns4.cloudflare.net.     111793  IN      AAAA    2400:cb00:2049:1::c629:df83
-ns5.cloudflare.net.     111793  IN      AAAA    2400:cb00:2049:1::c629:df1f
-
-;; Query time: 16 msec
-;; SERVER: 192.168.0.1#53(192.168.0.1) (UDP)
-;; WHEN: Sun Mar 03 16:05:17 AEDT 2024
-;; MSG SIZE  rcvd: 466
-```
-
 And the Authority section shows the available resource records (RR) to available authoritative name servers to the query. While the additional section shows RR's that relate to the query on my home-machine.
 
 ### 3.4) What is the IP address of the local nameserver for your machine?
 
-At the end of the dig output, I can see that my server IP address of the local name server for my VLAB machine is: 129.94.242.2
+At the end of the dig output, I can see that my server IP address of the local name server for my machine is: 129.94.242.2
 
 ### 3.5) What are the DNS nameservers for the " princeton.edu ” domain (note: the domain name is princeton.edu and not <www.princeton.edu> . This is an example of what is referred to as the apex/naked domain)? Find their IP addresses. Which DNS query type is used to obtain this information?
 
