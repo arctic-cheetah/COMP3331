3cd554c943a54064101abfc18b4003fe1f3bbc99
arctic-cheetah <gondonlondon@gmail.com>
2024-04-08 13:25:08 +1000

    FInished ex2 code

--- lab05/exercise2.tcl
+++ lab05/exercise2.tcl
@@ -1,4 +1,4 @@
-#Create a simulator object#>>
+#Create a simulator object#TODO:
 set ns [new Simulator]
 
 #Define different colors
@@ -30,7 +30,7 @@ proc finish {} {
     exec nam out.nam &
     # exec 3331 nam out.nam &
     # Execute gnuplot to display the two trace files tcp1.tr and tcp2.tr
-    #exec gnuplot throughput.plot &
+    exec gnuplot throughput.plot &
     exit 0
 }
 
@@ -54,8 +54,6 @@ $ns duplex-link $n4 $n6 2.5Mb 40ms DropTail
 $ns duplex-link $n4 $n5 10Mb 10ms DropTail
 $ns duplex-link $n6 $n7 10Mb 10ms DropTail
 
-
-
 # set the correct orientation for all nodes
 $ns duplex-link-op $n0 $n1 orient right
 $ns duplex-link-op $n0 $n1 orient right
@@ -71,7 +69,7 @@ $ns duplex-link-op $n1 $n2 orient up
 #Set Queue limit and Monitor the queue for the link between node 2 and node 4
 $ns queue-limit $n2 $n4 10
 $ns duplex-link-op $n2 $n4 queuePos 0.5
-
+#------------------------------------------------------------
 #Create a TCP agent and attach it to node n0
 set tcp1 [new Agent/TCP]
 $ns attach-agent $n0 $tcp1
@@ -87,57 +85,93 @@ $tcp1 set fid_ 1
 #Setup FTP over TCP connection
 set ftp1 [new Application/FTP]
 $ftp1 attach-agent $tcp1
-
+#------------------------------------------------------------
 #Create a TCP agent and attach it to node n3
-#>>
+#TODO:
+set tcp2 [new Agent/TCP]
+$ns attach-agent $n3 $tcp2
+
 #Sink for traffic at Node n5
-#>>
+#TODO:
+set sink2 [new Agent/TCPSink]
+$ns attach-agent $n5 $sink2
+
 #Connect
-#>>
+#TODO:
+$ns connect $tcp2 $sink2
+$tcp2 set fid_ 2
+
 #Setup FTP over TCP connection
-#>>
+#TODO:
+set ftp2 [new Application/FTP]
+$ftp2 attach-agent $tcp2 
+#------------------------------------------------------------
 
 #Create a TCP agent and attach it to node n7
-#>>
+#TODO:
+set tcp3 [new Agent/TCP]
+$ns attach-agent $n7 $tcp3
 
 #Sink for traffic at Node n0
-#>>
+#TODO:
+set sink3 [new Agent/TCPSink]
+$ns attach-agent $n0 $sink3
 
 #Connect
-#>>
+#TODO:
+$ns connect $tcp3 $sink3
+$tcp3 set fid_ 3
+
 
 #Setup FTP over TCP connection
-#>>
-
+#TODO:
+set ftp3 [new Application/FTP]
+$ftp3 attach-agent $tcp3
+#------------------------------------------------------------
 
 #Create a TCP agent and attach it to node n7
-#>>
+#TODO:
+set tcp4 [new Agent/TCP]
+$ns attach-agent $n7 $tcp4
 
 #Sink for traffic at Node n3
-#>>
+#TODO:
+set sink4 [new Agent/TCPSink]
+$ns attach-agent $n3 $sink4
 
 #Connect
-#>>
+#TODO:
+$ns connect $tcp4 $sink4
+$tcp4 set fid_ 4
 
 #Setup FTP over TCP connection
-#>>
+#TODO:
+set ftp4 [new Application/FTP]
+$ftp4 attach-agent $tcp4
+
+#------------------------------------------------------------
 
 proc record {} {
-#>>
+        global sink1 sink2 f1 f2
+        #TODO:
         #Get an instance of the simulator
         set ns [Simulator instance]
         #Set the time after which the procedure should be called again
         set time 0.1
         #How many bytes have been received by the traffic sinks at n5?
         set bw1 [$sink1 set bytes_]
-#>>
-	#Get the current time
+        set bw2 [$sink2 set bytes_]
+        #TODO:
+	    #Get the current time
         set now [$ns now]
         #Calculate the bandwidth (in MBit/s) and write it to the files
         puts $f1 "$now [expr $bw1/$time*8/1000000]"
         puts $f2 "$now [expr $bw2/$time*8/1000000]"
         #Reset the bytes_ values on the traffic sinks
-#>>
+        $sink1 set bytes_ 0
+        $sink2 set bytes_ 0
+
+        #TODO:
         #Re-schedule the procedure
         $ns at [expr $now+$time] "record"
 }
@@ -145,18 +179,26 @@ proc record {} {
 
 #Schedule events for all the agents
 #start recording
-#>>
+#TODO:
+$ns at 0 "record"
 
 #start FTP sessions
+#TODO:
 $ns at 0.5 "$ftp1 start"
-#>>
+$ns at 2.0 "$ftp2 start"
+$ns at 3.0 "$ftp3 start"
+$ns at 4.0 "$ftp4 start"
 
 #Stop FTP sessions
-#>>
+#TODO:
+$ns at 8.5 "$ftp1 stop"
+$ns at 9.5 "$ftp2 stop"
+$ns at 9.5 "$ftp3 stop"
+$ns at 7.0 "$ftp4 stop"
 
 #Call the finish procedure after 10 seconds of simulation time
 $ns at 10.0 "finish"
 
 #Run the simulation
-#>>
+#TODO:
 $ns run
